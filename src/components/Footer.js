98611280b7c422054918e7ab212488d0d2b86608
Isaiah Keller <isaiah.keller@gmail.com>
2022-07-12 16:57:17 -0700

    created Header Main and Footer js files and transfered the code from app.js into the individual files.

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+    return (
+        <footer>
+            <p>Author: Isaiah Keller</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+
+function Header() {
+    return (
+        <header className="App-header">
+        <h1>Horned Beasts</h1>
+      </header>
+    )
+}
+
+export default Header;
