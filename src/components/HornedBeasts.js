3eb78c6577621c7e691107007f0c631b39d0bffa
Isaiah Keller <isaiah.keller@gmail.com>
2022-07-13 16:30:02 -0700

    added data.js on, worked through retrieving data from the JSON link through axios, couldn't get it to work, transfering to simply referencing the data from a local file.

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,22 +1,29 @@
 import { Component } from 'react';
+// import Button from 'react-bootstrap/Button';
 
 
 class HornedBeast extends Component {
     constructor(props) {
         super(props);
+        console.log('Props: ')
         console.log(props);
+        console.log('--------')
+
         this.state = {
             favorite: 0,
         }
-      }
+    }
 
-      
-      render() {
+
+    render() {
         return (
-        <div id="beast-list">
-            {this.props.beasts.map(beast => <img className="card-img" width="200px" src={beast.imgUrl} alt={beast.title} />)}
-        </div>
-    )}
+            <div id="beast-list">
+                {/* {this.props.beasts.map(beast => <h3>{beast.title}</h3>)} */}
+                {this.props.beasts.map(beast => <img className="card-img" width="200px" src={beast.image_Url} alt={beast.title} />)}
+                {/* {this.props.beasts.map(beast => <h3>{beast.description}</h3>)} */}
+            </div>
+        )
+    }
 }
 
 export default HornedBeast;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,36 @@
 import { Component } from 'react';
 import HornedBeast from './HornedBeasts';
-import list from 'https://codefellows.github.io/code-301-guide/curriculum/class-02/lab/assets/data.json';
+import axios from 'axios';
+import list from '../data.json';
 
 class Main extends Component {
 
-    constructor() {
-        console.log(list)
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            currentBeast: { image_url: null },
+            currentBeast: {},
             beastsList: list,
-        }
+        };
     }
+
+    // retrieveData = () => {
+    //     let list = [];
+    //     axios.get('https://codefellows.github.io/code-301-guide/curriculum/class-02/lab/assets/data.json')
+    //         .then(response => {
+    //             list = JSON.stringify(response)
+    //             // console.log('JSON response: ')
+    //             // console.log(list);
+    //             // console.log('------------')
+    //             console.log('JSON.parse(list): ')
+    //             console.log(JSON.parse(list));
+    //             console.log('------------')
+    //             list = JSON.parse(list)
+    //             console.log('new list.data')
+    //             console.log(list.data);
+    //             console.log('------------')
+    //         });
+    // }
+
 
     //     function localStorageCreate() {
     //     let saveImages = JSON.stringify(images);
@@ -23,12 +42,23 @@ class Main extends Component {
     //     return JSON.parse(retrieveImages);
     // }
 
-render() {
-    return(
-    <main className = 'card-grid' >
-        <HornedBeast beasts={this.state.beastsList} />
-    </main>
-)}
+    render() {
+
+        if (this.state.beastsList) {
+
+            console.log('this.state.beastsList')
+            console.log(this.state.beastsList)
+            console.log('------------')
+        }
+        return (
+
+            <main className='card-grid' >
+                {this.state.beastsList && <p>{this.state.beastsList[0].title}</p>}
+                <HornedBeast beasts={this.state.beastsList} />
+
+            </main>
+        )
+    }
 }
 
 export default Main;
