1ee9b63c36158db3fe4460aed9d57efab9d5e69e
Isaiah Keller <isaiah.keller@gmail.com>
2022-07-13 10:14:24 -0700

    installed bootstrap

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -5,6 +5,9 @@ class HornedBeast extends Component {
     constructor(props) {
         super(props);
         console.log(props);
+        this.state = {
+            favorite: 0,
+        }
       }
 
       
